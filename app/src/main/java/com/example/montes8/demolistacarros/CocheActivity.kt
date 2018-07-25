6eb216ee798a34f10108ae1bcb7b8490f38177ea
montes8 <emontesinos812@gmail.com>
2018-07-25 15:31:19 -0500

    cargando los detales en los actvitys

--- app/src/main/java/com/example/montes8/demolistacarros/CocheActivity.kt
+++ app/src/main/java/com/example/montes8/demolistacarros/CocheActivity.kt
@@ -2,6 +2,7 @@ package com.example.montes8.demolistacarros
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_coche.*
 
 class CocheActivity : AppCompatActivity() {
 
@@ -9,6 +10,16 @@ class CocheActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coche)
 
-
+//recibiemos los valores que nos manda desde el activity y lo renderisamos
+        val bundle:Bundle=intent.extras
+        val imagen=bundle.getInt("imagen")
+        val titulo=bundle.getString("titulo")
+        val descripcion=bundle.getString("descripcion")
+        val precio=bundle.getString("precio")
+        //los rellenamos otra ves
+        image_view2.setImageResource(imagen)
+        text_titulo2.text=titulo
+        text_descripcion2.text=descripcion
+        text_precio2.text=precio.toString()
     }
 }
